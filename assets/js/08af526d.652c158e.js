9cd32da0d0e3157ef30b55d7f8d164e13a6f8d5b
Vinay Kagithapu <vinaykagithapu@Vinays-MacBook-Pro.local>
2026-08-08 11:57:38 +0530

    Deploy website - based on 6cb26f0a01181c85ded453a5fad5173b220360a9

--- assets/js/08af526d.652c158e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkvinay_portfolio=globalThis.webpackChunkvinay_portfolio||[]).push([[529],{1344(o){o.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
